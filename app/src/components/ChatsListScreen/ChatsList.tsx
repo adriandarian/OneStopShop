43e91a2380e8e31b42ea2e280a1804bde8230ed8
adriandarian <adarian@ucmerced.edu>
2020-11-15 00:56:07 -0800

    add: test scripts with jest

--- app/src/components/ChatsListScreen/ChatsList.tsx
+++ app/src/components/ChatsListScreen/ChatsList.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from 'react';
+import React, { useState, useMemo } from 'react';
 import moment from 'moment';
 import { List, ListItem } from '@material-ui/core';
 import styled from 'styled-components';
@@ -72,7 +72,7 @@ const getChatsQuery = `
 
 const ChatsList = () => {
   const [chats, setChats] = useState<any[]>([]);
- 
+
   useMemo(async () => {
     const body = await fetch(`${process.env.REACT_APP_SERVER_URL}/graphql`, {
       method: 'POST',
@@ -81,26 +81,32 @@ const ChatsList = () => {
       },
       body: JSON.stringify({ query: getChatsQuery }),
     });
-    
+
     const {
       data: { chats },
     } = await body.json();
 
     setChats(chats);
   }, []);
- 
+
   return (
     <Container>
       <StyledList>
         {chats.map((chat) => (
           <StyledListItem key={chat!.id} button>
-            <ChatPicture src={chat.picture} alt="Profile" />
+            <ChatPicture
+              data-testid="picture"
+              src={chat.picture}
+              alt="Profile"
+            />
             <ChatInfo>
-              <ChatName>{chat.name}</ChatName>
+              <ChatName data-testid="name">{chat.name}</ChatName>
               {chat.lastMessage && (
                 <React.Fragment>
-                  <MessageContent>{chat.lastMessage.content}</MessageContent>
-                  <MessageDate>
+                  <MessageContent data-testid="content">
+                    {chat.lastMessage.content}
+                  </MessageContent>
+                  <MessageDate data-testid="date">
                     {moment(chat.lastMessage.createdAt).format('HH:mm')}
                   </MessageDate>
                 </React.Fragment>
