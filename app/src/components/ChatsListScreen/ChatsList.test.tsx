4e95fb11a35abbf0adcf299a3458a818b0c4fdb8
adriandarian <adarian@ucmerced.edu>
2020-11-15 21:57:11 -0800

    add: app routing

--- app/src/components/ChatsListScreen/ChatsList.test.tsx
+++ app/src/components/ChatsListScreen/ChatsList.test.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { cleanup, render, waitFor } from '@testing-library/react';
+import {
+  cleanup,
+  render,
+  waitFor,
+  fireEvent,
+  screen,
+} from '@testing-library/react';
+import { createBrowserHistory } from 'history';
+
 import ChatsList from './ChatsList';
- 
+
 describe('ChatsList', () => {
-  afterEach(cleanup);
- 
+  afterEach(() => {
+    cleanup();
+
+    delete window.location;
+    window = Object.create(window);
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: '/',
+      },
+      writable: true,
+    });
+  });
+
   it('renders fetched chats data', async () => {
     fetchMock.mockResponseOnce(
       JSON.stringify({
@@ -25,19 +44,59 @@ describe('ChatsList', () => {
         },
       })
     );
- 
+
     {
-      const { container, getByTestId } = render(<ChatsList />);
- 
-      await waitFor(() => container);
- 
+      const history = createBrowserHistory();
+
+      const { container, getByTestId } = render(
+        <ChatsList history={history} />
+      );
+
+      await waitFor(() => screen.getByTestId('name'));
+
       expect(getByTestId('name')).toHaveTextContent('Foo Bar');
       expect(getByTestId('picture')).toHaveAttribute(
         'src',
         'https://localhost:4000/picture.jpg'
       );
-      expect(getByTestId('content')).toHaveTextContent('Hello');
       expect(getByTestId('date')).toHaveTextContent('00:00');
+    }
+  });
+
+  it('should navigate to the target chat room on chat item click', async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        data: {
+          chats: [
+            {
+              id: 1,
+              name: 'Foo Bar',
+              picture: 'https://localhost:4000/picture.jpg',
+              lastMessage: {
+                id: 1,
+                content: 'Hello',
+                createdAt: new Date('1 Jan 2019 GMT'),
+              },
+            },
+          ],
+        },
+      })
+    );
+
+    const history = createBrowserHistory();
+
+    {
+      const { container, getByTestId } = render(
+        <ChatsList history={history} />
+      );
+
+      await waitFor(() => screen.getByTestId('chat'));
+
+      fireEvent.click(getByTestId('chat'));
+
+      await waitFor(() =>
+        expect(history.location.pathname).toEqual('/chats/1')
+      );
     }
   });
 });
