8713ec719a58416639697e3d08823a3634ee6747
adriandarian <adarian@ucmerced.edu>
2020-11-17 16:40:57 -0800

    migrate to submodule

--- app/src/react-app-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react-scripts" />
-declare module 'react-router-transition';
-declare module 'apollo-cache-inmemory';
-declare module 'apollo-client';
-declare module 'apollo-link-http';
-declare module 'apollo-link-ws';
